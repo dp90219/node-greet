13e8967cfd4ea23c8a24f92b1803a851a5e985c0
liupeng <dp90219@gmail.com>
2014-04-29 13:11:40 +0800

    change argv._[0]

--- bin/greet.js
+++ bin/greet.js
@@ -5,6 +5,6 @@ var parseArgs = require('minimist')
 
 var argv = parseArgs(process.argv.slice(2))
 
-console.log(greet(argv._, argv.drunk))
+console.log(greet(argv._[0], argv.drunk))
 
 // console.log(process.argv[0])
